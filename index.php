d9737fd46e730aa4fe55ad611d99b32c51c3d416
Walid JAZAYERI <jazayeri.walid1@gmail.com>
2024-04-16 22:16:03 +0200

    ajout d'un lien

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
     <a href="https://biblio.devi.host">Projet bibliotheque personelle</a>
 </div>
 <div>
-    <a href="https://courspoo.devi.host">Projet Poo de Lior Chamla</a>
+    <a href="https://courspoo.devi.host">Projet Poo de Lior Chamla</a> <a href="https://github.com/WalidJazayeri/PooLior">Lien Github du projet</a>
 </div>
