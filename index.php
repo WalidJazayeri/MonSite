3ea6011e6f19982efd3aa70f15c727f24015da44
Walid JAZAYERI <jazayeri.walid1@gmail.com>
2024-04-16 22:09:08 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<h1>Bienvenue sur mon site</h1>
+<div>
+    <a href="https://biblio.devi.host">Projet bibliotheque personelle</a>
+</div>
+<div>
+    <a href="https://courspoo.devi.host">Projet Poo de Lior Chamla</a>
+</div>
